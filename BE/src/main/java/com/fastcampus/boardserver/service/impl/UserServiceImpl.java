3aac7c391295433f1fcd00dabfd78dec60155276
suyeon-hyeon <shonee9911@gmail.com>
2026-01-21 02:28:52 +0900

    [✨add] 로그인 구현 - config, util 설정 - controller, service, DTO, mapper 설정 - DB연결을 위한 gradle 및 yml 수정

---
+++ BE/src/main/java/com/fastcampus/boardserver/service/impl/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.fastcampus.boardserver.service.impl;
+
+import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.exception.DuplicateIdException;
+import com.fastcampus.boardserver.mapper.UserProfileMapper;
+import com.fastcampus.boardserver.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+import static com.fastcampus.boardserver.utils.SHA256Util.encryptSHA256;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private UserProfileMapper userProfileMapper;
+
+    public UserServiceImpl(UserProfileMapper userProfileMapper) {
+        this.userProfileMapper = userProfileMapper;
+    }
+
+    @Override
+    public void register(UserDTO userProfile) {
+        boolean duplIdResult = isDuplicatedId(userProfile.getUserId());
+        if (duplIdResult) {
+            throw new DuplicateIdException("중복된 아이디입니다.");
+        }
+        userProfile.setCreateTime(new Date());
+        userProfile.setPassword(encryptSHA256(userProfile.getPassword()));
+    }
+
+    @Override
+    public UserDTO login(String id, String password) {
+        return null;
+    }
+
+    @Override
+    public boolean isDuplicatedId(String id) {
+        return false;
+    }
+
+    @Override
+    public UserDTO getUserInfo(String userId) {
+        return null;
+    }
+
+    @Override
+    public void updatePassword(String id, String beforePassword, String afterPassword) {
+
+    }
+
+    @Override
+    public void deleteId(String id, String password) {
+
+    }
+}
