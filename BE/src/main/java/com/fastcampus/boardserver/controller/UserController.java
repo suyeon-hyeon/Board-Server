8a877312ddcead081b84deab1f9c94de3ae088ad
suyeon-hyeon <shonee9911@gmail.com>
2026-01-24 05:23:34 +0900

    [✨add] 로그인 구현 - 로그인 controller

--- BE/src/main/java/com/fastcampus/boardserver/controller/UserController.java
+++ BE/src/main/java/com/fastcampus/boardserver/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.fastcampus.boardserver.controller;
 
 import com.fastcampus.boardserver.dto.UserDTO;
+import com.fastcampus.boardserver.dto.request.UserLoginRequest;
+import com.fastcampus.boardserver.dto.response.LoginResponse;
 import com.fastcampus.boardserver.service.impl.UserServiceImpl;
+import com.fastcampus.boardserver.utils.SessionUtil;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,5 +30,31 @@ public class UserController {
             throw new RuntimeException("회원가입 정보를 확인해주세요");
         }
         userService.register(userDTO);
+    }
+
+    @PostMapping("sign-in")
+    public HttpStatus login(@RequestBody UserLoginRequest userLoginRequest,
+                            HttpSession session) {
+        ResponseEntity<LoginResponse> responseEntity = null;
+        String id = userLoginRequest.getUserId();
+        String password = userLoginRequest.getPassword();
+        LoginResponse loginResponse;
+        UserDTO userInfo = userService.login(id,password);
+
+        if(userInfo == null) {
+            return HttpStatus.NOT_FOUND;
+        } else if (userInfo != null) {
+            loginResponse = LoginResponse.success(userInfo);
+            if(userInfo.getStatus() == (UserDTO.Status.ADMIN))
+                SessionUtil.setLoginAdminId(session, id);
+            else
+                SessionUtil.setLoginMemberId(session, id);
+
+            responseEntity = new ResponseEntity<LoginResponse>(loginResponse, HttpStatus.OK);
+        } else {
+            throw new RuntimeException("Login Error! 유저 정보가 없거나 지워진 유저 정보입니다.");
+        }
+
+        return HttpStatus.OK;
     }
 }
