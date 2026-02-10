2167090c40379318e023cb904c0efe866aa62f5b
suyeon-hyeon <shonee9911@gmail.com>
2026-02-10 23:28:17 +0900

    [✨add] 카테고리 API

---
+++ BE/src/main/java/com/fastcampus/boardserver/dto/request/CategoryRequest.java
@@ -0,0 +1,11 @@
+package com.fastcampus.boardserver.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryRequest {
+    private int id;
+    private String name;
+}
