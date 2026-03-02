e3ad980f35340f6c74c76ddd89b57dd9093c446a
suyeon-hyeon <shonee9911@gmail.com>
2026-03-02 23:00:59 +0900

    [✨Add] 태그 기능 API CRUD

--- BE/src/main/java/com/fastcampus/boardserver/service/PostService.java
+++ BE/src/main/java/com/fastcampus/boardserver/service/PostService.java
@@ -2,6 +2,7 @@ package com.fastcampus.boardserver.service;
 
 import com.fastcampus.boardserver.dto.CommentDTO;
 import com.fastcampus.boardserver.dto.PostDTO;
+import com.fastcampus.boardserver.dto.TagDTO;
 
 import java.util.List;
 
@@ -13,4 +14,7 @@ public interface PostService {
     void registerComment(CommentDTO commentDTO);
     void updateComment(CommentDTO commentDTO);
     void deletePostComment(int userId, int commentId);
+    void registerTag(TagDTO tagDTO);
+    void updateTag(TagDTO tagDTO);
+    void deletePostTag(int userId, int tagId);
 }
